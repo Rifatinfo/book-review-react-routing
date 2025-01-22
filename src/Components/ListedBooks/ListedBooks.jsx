7bb01cf53dbda7fb0e06292180eefbdaf949d6b6
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-01-22 23:59:19 +0600

    sorting funtion create but some problem issues create

--- src/Components/ListedBooks/ListedBooks.jsx
+++ src/Components/ListedBooks/ListedBooks.jsx
@@ -6,9 +6,17 @@ import 'react-tabs/style/react-tabs.css';
 import { getStoredReadList } from '../../Utility/localStorage';
 import Book from '../Books/Book';
 const ListedBooks = () => {
+    const [readList, setReadList] = useState([]);   
+    const [sort, setSort] = useState('');
+    const handleSorting = (sortByItem) =>{
+        setSort(sortByItem);
+        if(sortByItem === "Rating"){
+           const ratingSort = [...readList].sort((a,b) => a.rating - b.rating);
+           setSort(ratingSort);
+        }
+    }
     const allBooks = useLoaderData();
     console.log(allBooks);
-    const [readList, setReadList] = useState([]);   
     useEffect(() => {
        const storedReadList = getStoredReadList();
        const storedReadListInt = storedReadList.map(id => parseInt(id));
@@ -23,6 +31,16 @@ const ListedBooks = () => {
     return (
         <div>
             <h1 className='text-center'></h1>
+            <div className='flex items-center justify-center'>
+            <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn m-1">{sort ? `${sort}` : "sort By" }</div>
+            <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                <li onClick={() => handleSorting("Rating")}><a>Rating</a></li>
+                <li onClick={() => handleSorting("Number of pages")}><a>Number of pages</a></li>
+                <li onClick={() => handleSorting("Publisher year")}><a>Publisher year</a></li>
+            </ul>
+            </div>
+            </div>
             <Tabs>
                 <TabList>
                     <Tab>Read Book</Tab>
